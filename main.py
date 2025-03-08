51ba9fd59bee52320cc4c55e0c04125d90132fca
Fooot <59772241+Fooot-Code@users.noreply.github.com>
2025-03-08 09:55:41 -0700

    Reverted name change

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def main():
         list[list] - List of all of the lists for the publishers and subscribers
         """
 
-        reefTable = visionTable.getTable("CoralLocationTable")
+        reefTable = visionTable.getTable("ReefLocationTable")
         reefL1Topic = reefTable.getBooleanArrayTopic("ReefL1")
         reefL2Topic = reefTable.getBooleanArrayTopic("ReefL2")
         reefL3Topic = reefTable.getBooleanArrayTopic("ReefL3")
