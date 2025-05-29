a600b232b41d858629250e25be4125b78e8c12e6
oof15642 <59772241+Fooot-Code@users.noreply.github.com>
2025-05-29 14:47:50 -0600

    INDENT

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ def main():
                     else:
                         robotBackPosePublisher.set(Pose3d(Translation3d(0, 0, 0), Rotation3d(0, 0, 0)))
 
-    time.sleep(0.01) # 10 ms
+        time.sleep(0.01) # 10 ms
 
             
 if __name__ == "__main__":
