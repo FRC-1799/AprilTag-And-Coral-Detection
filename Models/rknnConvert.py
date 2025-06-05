59eeb717087a8c55f6c8ef30cd32edc83bcfe3dd
frc1799printer <frc1799printer@gmail.com>
2025-06-05 14:15:00 -0600

    RKNN with quantization didnt work, will retrain model with actual images sizes

--- Models/rknnConvert.py
+++ Models/rknnConvert.py
@@ -2,39 +2,39 @@ from ultralytics import YOLO
 from rknn.api import RKNN
 
 # Converts trained model to ONNX format
-model = YOLO("Models/BestModel.pt")
-model.export(format="onnx",opset=12, simplify=True)
+# model = YOLO("Models/BestModel.pt")
+# model.export(format="onnx",opset=12, simplify=True)
 
 # Paths
-ONNX_MODEL = './BestModel.onnx'
-RKNN_MODEL = './Test-640-640-yolov8n.rknn' # TODO: Change this later to be the model name
-DATASET_PATH = './dataset.txt'
+ONNX_MODEL = "Models/BestModel.onnx"
+RKNN_MODEL_NAME = "Models/ReefV1-640-640-yolov8n.rknn" # TODO: Change this later to be the model name
+DATASET_PATH = "Models/dataset.txt"
 
 # Initialize RKNN object
 rknn = RKNN(verbose=True)
 
-# Configuration for RK3588. This is the main chip used with PhotonVision, which is used in most Orange Pi's (citation needed)
+# Configuration for RK3588. This is the main chip used with PhotonVision, which is used in most Orange Pis (citation needed)
 rknn.config(
-    target_platform='rk3588',
+    target_platform="rk3588",
     mean_values=[[0, 0, 0]],
     std_values=[[255, 255, 255]],
-    quantized_dtype='asymmetric_affine-u8'
+    quantized_dtype="w8a8"
 )
 
 # Load ONNX model
 ret = rknn.load_onnx(model=ONNX_MODEL)
 if ret != 0:
-    print('Failed to load ONNX model.')
+    print("Failed to load ONNX model.")
     exit(ret)
 
 # Build the RKNN model with quantization
 ret = rknn.build(do_quantization=True, dataset=DATASET_PATH)
 if ret != 0:
-    print('Failed to build RKNN model.')
+    print("Failed to build RKNN model.")
     exit(ret)
 
 # Export the RKNN model
-ret = rknn.export_rknn(RKNN_MODEL)
+ret = rknn.export_rknn(RKNN_MODEL_NAME)
 if ret != 0:
-    print('Failed to export RKNN model.')
+    print("Failed to export RKNN model.")
     exit(ret)
--- quick.py
+++ quick.py
@@ -1,34 +1,14 @@
-# import os
-
-# folder = "C:\\Users\\brenn\\Downloads\\Images For Reef Detection.v1i.yolov8\\train\\images"
-
-# for filename in os.listdir(folder):
-#     if filename.endswith(".jpg"):
-#         # Get the part before the first underscore
-#         new_name = filename.split("_")[0] + ".jpg"
-
-#         # Build full file paths
-#         src = os.path.join(folder, filename)
-#         dst = os.path.join(folder, new_name)
-
-#         # Rename the file
-#         os.rename(src, dst)
-#         print(f"Renamed: {filename} -> {new_name}")
-
 import os
+imageFolder = "/home/lidar/Downloads/2025 REEFSCAPE.v1i.yolov8/test/images/"
+outputFile = "dataset.txt"
+validExts = [".jpg"]
 
-folder = "C:\\Users\\brenn\\Downloads\\Images For Reef Detection.v1i.yolov8\\test\\images"
+imagePaths = [
+    os.path.join(imageFolder, fname)
+    for fname in os.listdir(imageFolder)
+    if os.path.splitext(fname)[1].lower() in validExts
+]
 
-for filename in os.listdir(folder):
-    if ".jpg" in filename:
-        # Remove all occurrences of '.jpg' (case-insensitive), then add a single one
-        name_without_ext = filename.lower().replace("frame_", "")
-        #new_name = name_without_ext + ".jpg"
-
-        src = os.path.join(folder, filename)
-        #dst = os.path.join(folder, new_name)
-
-        # Only rename if name actually changes
-        if src != name_without_ext:
-            os.rename(src, name_without_ext)
-            print(f"Renamed: {filename} -> {name_without_ext}")
+with open(outputFile, "w") as f:
+    for path in imagePaths:
+        f.write(path + "\n")
